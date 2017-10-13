9ebf91a337b3e3666df8b52f201ad4f87c2e4494
sophy23 <sofiat.abioye@andela.com>
2017-10-13 16:27:16 +0100

    refactor(heroku) make changes on app dot js for heroku hosting

--- app.js
+++ app.js
@@ -15,34 +15,34 @@ const app = express();
 
 // Log requests to the console.
 app.use(logger('dev'));
-// const compiler = webpack(webpackConfig);
+const compiler = webpack(webpackConfig);
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text({ type: 'text/html' }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
-// app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(path.join(__dirname, './client')));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-// app.use(webpackMiddleware(compiler));
+app.use(webpackMiddleware(compiler));
 
-// app.use(webpackHotMiddleware(compiler, {
-//     hot: true,
-//     noInfo: true,
-//     publicPath: webpackConfig.output.publicPath,
-// }));
-
-routes(app, authorize, checkadmin);
-app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
+app.use(webpackHotMiddleware(compiler, {
+    hot: true,
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
 }));
 
-// app.get('*', (req, res) => {
-//     res.status(404).sendFile(path.join(__dirname, './client/public', 'index.html'));
-// });
+// routes(app, authorize, checkadmin);
+// app.get('*', (req, res) => res.status(200).send({
+//     message: 'Welcome to the beginning of nothingness.',
+// }));
 
-// app.post('*', (req, res) => {
-//     res.status(404).send({ message: 'Ooops, Couldnt find the url' });
-// });
+app.get('*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, './client/public', 'index.html'));
+});
+
+app.post('*', (req, res) => {
+    res.status(404).send({ message: 'Ooops, Couldnt find the url' });
+});
 
 module.exports = app;
