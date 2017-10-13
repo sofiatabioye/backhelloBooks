3dc6e19b7763f91e9647450e499674df6ee8d9b5
sophy23 <sofiat.abioye@andela.com>
2017-10-13 17:51:26 +0100

    bug(heroku) fix issues with build

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(webpackHotMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath,
 }));
 
-// routes(app, authorize, checkadmin);
+routes(app, authorize, checkadmin);
 // app.get('*', (req, res) => res.status(200).send({
 //     message: 'Welcome to the beginning of nothingness.',
 // }));
